1db33cb8ad191502804145bdc431ddceec245f27
root <root@istore2211.mylabserver.com>
2017-10-30 08:58:11 +0000

    wtf

---
+++ src/main/java/com/istore221/App.java
@@ -0,0 +1,17 @@
+package com.istore221;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(testMethod() );
+    }
+
+    public static String testMethod(){
+	return "Hello World";
+    }
+}
